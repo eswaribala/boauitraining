0b1a763a36d89fa228816ad7db5676a607cc2214
eswaribala <parameswaribala@gmail.com>
2021-08-25 15:31:27 +0530

    updated

---
+++ day1training/jsbasics/scripts/objectdemo.js
@@ -0,0 +1,10 @@
+//parameterized constructor
+function Customer(id,name,phoneNumber){
+    this.id=id;
+    this.name=name;
+    this.phoneNumber=phoneNumber;
+}
+
+//create the object
+var customer=new Customer(483658,'Arun',945699854698);
+console.log(customer);
